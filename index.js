826a882a20cf60475ba5787ef2aad6cb7ebef976
pritamcn <pritams@capitalnumbers.com>
2022-12-23 23:19:29 +0530

    up

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import cors from "cors";
 import { errorHandler, notFound } from "./middleware/errormiddleware.js";
 import dataroutes from "./routes/dataroutes.js";
-import pkg from "xlsx";
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 const app = express()
+app.use(cors());
   app.use(express.json());
+ 
   if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
   }
